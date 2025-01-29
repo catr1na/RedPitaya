eb156b16231623fa66f1d9d5e7b367705c299ac0
Daniel Campos <165080916+dacampos37@users.noreply.github.com>
2025-01-29 18:03:52 -0500

    Update Makefile.c

--- Examples/C/Daniel_thesis/Makefile.c
+++ Examples/C/Daniel_thesis/Makefile.c
@@ -2,15 +2,21 @@
 CC = gcc
 CFLAGS = -g -std=gnu11 -Wall -O2
 CFLAGS += -I/opt/redpitaya/include
-CFLAGS += -I/usr/local/include              # FFTW3 headers
+CFLAGS += -I/usr/local/include  # FFTW3 headers
 
 # Library paths and libraries
 LDFLAGS = -L/opt/redpitaya/lib
 LDFLAGS += -L/usr/local/lib
 LDLIBS = -lm -lpthread -lrp -lfftw3f
 
-# Source files
-SRCS = spectrogram_acquisition.c spec_dsp.c bubble_detector.c
+# Determine if CNN should be included based on environment variable
+ifeq ($(MODEL_PATH),)
+    SRCS = spectrogram_acquisition.c spec_dsp.c  # No CNN
+else
+    SRCS = spectrogram_acquisition.c spec_dsp.c bubble_detector.c  # Include CNN
+    CFLAGS += -DCNN_ENABLED  # Define CNN_ENABLED for conditional compilation
+endif
+
 OBJS = $(SRCS:.c=.o)
 TARGET = spectrogram_acquisition
 
