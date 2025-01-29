5b7a575e74d4f352fe13492a0b64ecc24c2dc22b
Daniel Campos <165080916+dacampos37@users.noreply.github.com>
2025-01-29 18:03:32 -0500

    Update bubble_detector.h

--- Examples/C/Daniel_thesis/bubble_detector.h
+++ Examples/C/Daniel_thesis/bubble_detector.h
@@ -4,8 +4,10 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+// CNN Architecture Parameters
 #define CONV1_FILTERS 32
 #define CONV2_FILTERS 64
+#define CONV3_FILTERS 128
 #define DENSE1_UNITS 128
 #define CONV_KERNEL_SIZE 3
 #define POOL_SIZE 2
@@ -15,7 +17,7 @@ typedef enum {
     DETECTION_BUBBLE = 1
 } DetectionResult;
 
-// Initialize detector with weights
+// Initialize detector with pre-trained weights
 bool detector_init(const char* weights_dir);
 
 // Process single spectrogram frame
@@ -23,8 +25,5 @@ DetectionResult detector_process_frame(float* spectrogram, int size);
 
 // Clean up detector resources
 void detector_cleanup(void);
-
-// Get the required input dimensions for the model
-void detector_get_input_dims(int* height, int* width);
 
 #endif // BUBBLE_DETECTOR_H
